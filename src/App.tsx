1b5b4e15d3f291d2d6cda4c8135ec4d3f62c4221
alex-glomo <alexandr@glomo.se>
2020-06-01 14:25:57 +0200

    State management: get data from state.

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,20 @@ import React from 'react';
 
 import { AppContainer } from './styles';
 import { Column } from './Column';
-import { Card } from './Card';
 import { AddNewItem } from './AddNewItem';
+import { List, useAppState } from './AppStateContext';
 
 function App() {
-	return (
-		<AppContainer>
-			<Column text={'To Do'}>
-				<Card text={'Generate app scaffold'}/>
-			</Column>
-			<Column text={'In Progress'}>
-				<Card text={'Learn TypeScript'}/>
-			</Column>
-			<Column text={'Done'}>
-				<Card text={'Begin to use static typing'}/>
-			</Column>
-			<AddNewItem toggleButtonText={'+ Add another list'} onAdd={console.log}/>
-		</AppContainer>
-	);
+  const { state } = useAppState();
+
+  return (
+    <AppContainer>
+      {state.lists.map((list: List, i: number) => (
+				<Column key={list.id} text={list.text} index={i}/>
+      ))}
+      <AddNewItem toggleButtonText={'+ Add another list'} onAdd={console.log}/>
+    </AppContainer>
+  );
 }
 
 export default App;
--- src/Column.tsx
+++ src/Column.tsx
@@ -2,17 +2,24 @@ import React from 'react';
 
 import { ColumnContainer, ColumnTitle } from './styles';
 import { AddNewItem } from './AddNewItem';
+import { useAppState } from './AppStateContext';
+import { Card } from './Card';
 
 interface ColumnProps {
-	text: string;
+  text: string;
+  index: number;
 }
 
-export const Column: React.FC<React.PropsWithChildren<ColumnProps>> = ({ text, children }) => {
-	return (
-		<ColumnContainer>
-			<ColumnTitle>{text}</ColumnTitle>
-			{children}
-			<AddNewItem toggleButtonText={'+ Add another task'} onAdd={console.log} dark={true}/>
-		</ColumnContainer>
-	);
+export const Column: React.FC<ColumnProps> = ({ text, index }) => {
+  const { state } = useAppState();
+
+  return (
+    <ColumnContainer>
+      <ColumnTitle>{text}</ColumnTitle>
+      {state.lists[index].tasks.map(task => (
+        <Card text={task.text} key={task.id} />
+      ))}
+      <AddNewItem toggleButtonText={'+ Add another task'} onAdd={console.log} dark={true}/>
+    </ColumnContainer>
+  );
 };
