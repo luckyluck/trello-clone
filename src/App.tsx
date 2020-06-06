8c4c2c685cb13e1242ee847aa98ab1cee2b129f2
Alexandr Ketov <ketov.alex.v@gmail.com>
2020-06-06 21:09:29 +0200

    Custom drag layer.

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,23 @@ import { AppContainer } from './styles';
 import { Column } from './Column';
 import { AddNewItem } from './AddNewItem';
 import { List, useAppState } from './AppStateContext';
+import { CustomDragLayer } from './CustomDragLayer';
 
 function App() {
-  const { state, dispatch } = useAppState();
+	const { state, dispatch } = useAppState();
 
-  return (
-    <AppContainer>
-      {state.lists.map((list: List, i: number) => (
-        <Column key={list.id} text={list.text} index={i} id={list.id}/>
-      ))}
-      <AddNewItem
-        toggleButtonText={'+ Add another list'}
-        onAdd={text => dispatch({ type: 'ADD_LIST', payload: text })}
-      />
-    </AppContainer>
-  );
+	return (
+		<AppContainer>
+			<CustomDragLayer/>
+			{state.lists.map((list: List, i: number) => (
+				<Column key={list.id} text={list.text} index={i} id={list.id}/>
+			))}
+			<AddNewItem
+				toggleButtonText={'+ Add another list'}
+				onAdd={text => dispatch({ type: 'ADD_LIST', payload: text })}
+			/>
+		</AppContainer>
+	);
 }
 
 export default App;
