878307952807dfdd745de922d3ac032dcd105360
Alexandr Ketov <ketov.alex.v@gmail.com>
2020-06-06 20:01:56 +0200

    Move the column.

--- src/Column.tsx
+++ src/Column.tsx
@@ -1,35 +1,51 @@
 import React, { useRef } from 'react';
+import { useDrop } from 'react-dnd';
 
 import { ColumnContainer, ColumnTitle } from './styles';
 import { AddNewItem } from './AddNewItem';
 import { useAppState } from './AppStateContext';
 import { Card } from './Card';
 import { useItemDrag } from './hooks/useItemDrag';
+import { DragItem } from './DragItem';
 
 interface ColumnProps {
-  text: string;
-  index: number;
-  id: string;
+	text: string;
+	index: number;
+	id: string;
 }
 
 export const Column: React.FC<ColumnProps> = ({ text, index, id }) => {
-  const { state, dispatch } = useAppState();
-  const ref = useRef<HTMLDivElement>(null);
-  const { drag } = useItemDrag({ type: 'COLUMN', id, index, text });
+	const { state, dispatch } = useAppState();
+	const ref = useRef<HTMLDivElement>(null);
+	const { drag } = useItemDrag({ type: 'COLUMN', id, index, text });
+	const [, drop] = useDrop({
+		accept: 'COLUMN',
+		hover(item: DragItem) {
+			const dragIndex = item.index;
+			const hoverIndex = index;
 
-  drag(ref);
+			if (dragIndex === hoverIndex) {
+				return;
+			}
 
-  return (
-    <ColumnContainer ref={ref}>
-      <ColumnTitle>{text}</ColumnTitle>
-      {state.lists[index].tasks.map(task => (
-        <Card text={task.text} key={task.id}/>
-      ))}
-      <AddNewItem
-        toggleButtonText={'+ Add another task'}
-        onAdd={text => dispatch({ type: 'ADD_TASK', payload: { text, taskId: state.lists[index].id } })}
-        dark={true}
-      />
-    </ColumnContainer>
-  );
+			dispatch({ type: 'MOVE_LIST', payload: { dragIndex, hoverIndex } });
+			item.index = hoverIndex;
+		}
+	});
+
+	drag(drop(ref));
+
+	return (
+		<ColumnContainer ref={ref}>
+			<ColumnTitle>{text}</ColumnTitle>
+			{state.lists[index].tasks.map(task => (
+				<Card text={task.text} key={task.id}/>
+			))}
+			<AddNewItem
+				toggleButtonText={'+ Add another task'}
+				onAdd={text => dispatch({ type: 'ADD_TASK', payload: { text, taskId: state.lists[index].id } })}
+				dark={true}
+			/>
+		</ColumnContainer>
+	);
 };
