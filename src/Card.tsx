d30469d4a0766f3f56992ef96ca0cc60aa0a8e72
Alexandr Ketov <ketov.alex.v@gmail.com>
2020-07-10 22:52:17 +0200

    Implement the useDrop.

--- src/Card.tsx
+++ src/Card.tsx
@@ -1,11 +1,54 @@
-import React from "react";
+import React, { useRef } from 'react';
+import { useDrop } from 'react-dnd';
 
-import { CardContainer } from "./styles";
+import { CardContainer } from './styles';
+import { CardDragItem } from './DragItem';
+import { useAppState } from './AppStateContext';
+import { useItemDrag } from './hooks/useItemDrag';
+import { isHidden } from './utils/isHidden';
 
 interface CardProps {
-    text: string;
+	text: string
+	index: number
+	id: string
+	columnId: string
+	isPreview?: boolean
 }
 
-export const Card: React.FC<CardProps> = ({ text }) => {
-    return <CardContainer>{text}</CardContainer>;
+export const Card: React.FC<CardProps> = ({ text, index, id, columnId, isPreview }) => {
+	const { state, dispatch } = useAppState()
+	const ref = useRef<HTMLDivElement>(null)
+	const { drag } = useItemDrag({ type: "CARD", id, index, text, columnId })
+	const [, drop] = useDrop({
+		accept: 'CARD',
+		hover(item: CardDragItem) {
+			if (item.id === id) {
+				return;
+			}
+
+			const dragIndex = item.index;
+			const hoverIndex = index;
+			const sourceColumn = item.columnId;
+			const targetColumn = columnId;
+
+			dispatch({
+				type: 'MOVE_TASK',
+				payload: { dragIndex, hoverIndex, sourceColumn, targetColumn }
+			});
+			item.index = hoverIndex;
+			item.columnId = targetColumn;
+		}
+	});
+
+	drag(drop(ref));
+
+	return (
+		<CardContainer
+			isHidden={isHidden(isPreview, state.draggedItem, "CARD", id)}
+			isPreview={isPreview}
+			ref={ref}
+		>
+			{text}
+		</CardContainer>
+	);
 };
